a94c39f98728bf3a2054a0c2e6e904e81dc11db2
Jake Westling <jakewestling@gmail.com>
2021-03-23 16:18:43 -0400

    Built Placeholder Logo. Splash screen responsive.

--- module/splash.js
+++ module/splash.js
@@ -1,11 +1,11 @@
 // Fade and remove splash element
-const body = document.querySelector('body');
-body.style.overflow = 'hidden';
-const splash = document.querySelector('#splash');
-const splashFade = setTimeout(function () {
-  splash.style.opacity = 0;
-  const splashZ = setTimeout(function () {
-    splash.remove();
-    body.style.overflow = 'visible';
-  }, 500);
-}, 1000);
+// const body = document.querySelector('body');
+// body.style.overflow = 'hidden';
+// const splash = document.querySelector('#splash');
+// const splashFade = setTimeout(function () {
+//   splash.style.opacity = 0;
+//   const splashZ = setTimeout(function () {
+//     splash.remove();
+//     body.style.overflow = 'visible';
+//   }, 500);
+// }, 1000);
